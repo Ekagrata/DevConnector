e4ee79aefdbc5d2ed9637340612d2b5cdbe9e270
Ekagrata <ekagratasharma@gmail.com>
2020-06-20 12:38:54 +0530

    User Authentication Added

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
-
-const app = express();
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const app = express();
+
+//connect db
+connectDB();
+
+//Init Middleware
+
+app.use(express.json({ extended: false }));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 app.get('/', (req, res) => res.send(`API Running`));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
